6c5c662a6a6cc31a90d936b6d850be5a06cdb9a3
shishkin015 <shishkin015@gmail.com>
2023-11-02 19:09:58 +0300

    Сырой проект сервиса по рассылке писем

--- messemail/admin.py
+++ messemail/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from messemail.models import Client
+
+
 # Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email',)
